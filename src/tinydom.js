bfd7d72a89bb0b93fcb8a2f1f500f4e814049952
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-10-22 12:46:21 +0100

    Add development and production scripts

---
+++ src/tinydom.js
@@ -0,0 +1,60 @@
+/*! tinyDOM v0.0.0 | (c) 2020 Kieran Barker | MIT License | https://github.com/kieranbarker/tinyDOM */
+const $ = class TinyDOM {
+
+  /**
+   * Create a new TinyDOM instance
+   * @param {String} selector A CSS selector string
+   */
+  constructor (selector) {
+    this.nodes = [...document.querySelectorAll(selector)];
+  }
+
+  /**
+   * Get an immutable copy of the matching elements
+   * @returns {Element[]} An immutable copy of the matching elements
+   */
+  items () {
+    return [...this.nodes];
+  }
+
+  /**
+   * Get the first matching element
+   * @returns {Element} The first matching element
+   */
+  first () {
+    return this.nodes[0];
+  }
+
+  /**
+   * Get the last matching element
+   * @returns {Element} The last matching element
+   */
+  last () {
+    return this.nodes[this.nodes.length - 1];
+  }
+
+  /**
+   * Add a class to all matching elements
+   * @param {String} className The class to be added
+   */
+  addClass (className) {
+    this.items().forEach(function (item) {
+      item.classList.add(className);
+    });
+
+    return this;
+  }
+
+  /**
+   * Remove a class from all matching elements
+   * @param {String} className The class to be removed
+   */
+  removeClass (className) {
+    this.items().forEach(function (item) {
+      item.classList.remove(className);
+    });
+
+    return this;
+  }
+
+}
